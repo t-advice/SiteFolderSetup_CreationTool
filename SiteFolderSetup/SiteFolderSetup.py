7f5424aa47db763b6c838ed2878dba15f675d46f
Tashwill <tashwilladvice02@gmail.com>
2026-05-19 19:29:02 +0200

    3. Combining the base path with the project which will create my Folder

--- SiteFolderSetup/SiteFolderSetup.py
+++ SiteFolderSetup/SiteFolderSetup.py
@@ -7,3 +7,11 @@ base_path = r"C:\Users\USER\Downloads"
 
 # 2. What will i name this folder 
 project_name = "NEW_UWC_PROJECT"
+
+
+# 3. Combining the base path with the project which will create my Folder
+full_project_path = os.path.join(base_path, project_name)
+
+
+
+   
