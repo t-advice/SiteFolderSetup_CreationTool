191050f1bed939bf0c402b3b4e4d45124761b2e3
Tashwill <tashwilladvice02@gmail.com>
2026-05-19 19:52:10 +0200

    threw a exception error , so i added a 's' LOL I  spend a few minutes with this  but it was a simple syntax error

--- SiteFolderSetup/SiteFolderSetup.py
+++ SiteFolderSetup/SiteFolderSetup.py
@@ -1,5 +1,5 @@
 import os 
-print)"Tashwill's Automation Tool: Folder Creator")
+print("Tashwill's Automation Tool: Folder Creator")
 print("-" *30)
 
 # 1. Where do i create the folder
@@ -13,7 +13,7 @@ project_name = "NEW_UWC_PROJECT"
 full_project_path = os.path.join(base_path, project_name)
 
 # 4. I need to check if the folder already exist.If it doesnt, it must be created.
-if not os.path.exist(full_project_path):
+if not os.path.exists(full_project_path):
     os.makedirs(full_project_path)
     print(f"Created project folder at:{full_project_path}")
 
