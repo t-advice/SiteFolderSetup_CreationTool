e055ecb07c03317f69a0f9b6dad0569d3c687d68
Tashwill <tashwilladvice02@gmail.com>
2026-05-19 19:34:31 +0200

    I need to check if the folder already exist.If it doesnt, it must be created. If i want to create some subfolders within main project

--- SiteFolderSetup/SiteFolderSetup.py
+++ SiteFolderSetup/SiteFolderSetup.py
@@ -12,6 +12,21 @@ project_name = "NEW_UWC_PROJECT"
 # 3. Combining the base path with the project which will create my Folder
 full_project_path = os.path.join(base_path, project_name)
 
+# 4. I need to check if the folder already exist.If it doesnt, it must be created.
+if not os.path.exist(full_project_path):
+    os.makedirs(full_project_path)
+    print(f"Created project folder at:{full_project_path}")
+
+else:
+    print("Main project folder already exists!")
+print("-" *30)
+
+
+# 5. If i want to create some subfolders within main project
+sub_folders = ["Invoices", "Site_Photos","Daily_Logs"]
+
+
+
 
 
    
